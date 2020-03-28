e6377102871f787a6b97c3238c4f053664679f9e
SamGearou <sgearou@atlassian.com>
2020-03-28 13:51:12 -0500

    notes about counting sort

--- src/main/java/sortingalgorithms/CountingSort.java
+++ src/main/java/sortingalgorithms/CountingSort.java
@@ -4,6 +4,7 @@ package sortingalgorithms;
 //0 to k, for some integer k. When k = O(n), the sort runs in Theta(n) time
 public class CountingSort {
 
+    //stable sort, time complexity: O(n + k), where n is A.length, and k = max elem in A
     public int[] countingSort(int[] A, int[] B, int k) {
         if (k < 1 || A == null || B == null) {
             return null;
@@ -15,6 +16,7 @@ public class CountingSort {
         for (int i = 1; i <= k; i++) {
             C[i] += C[i - 1];
         }
+        //could go from i = 0 to B.length-1, but this would make the algorithm not stable
         for (int i = B.length - 1; i >= 0; i--) {
             B[C[A[i]] - 1] = A[i];
             C[A[i]]--;
