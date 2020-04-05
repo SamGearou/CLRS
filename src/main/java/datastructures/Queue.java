193dfdb9fd33233d17b4312fd157a6fa4c7284c6
SamGearou <sgearou@atlassian.com>
2020-04-04 19:12:58 -0500

    generic queue implementation

---
+++ src/main/java/datastructures/Queue.java
@@ -0,0 +1,46 @@
+package datastructures;
+
+public class Queue<T extends Object> {
+    T[] queue;
+    int start;
+    int end;
+    int count = 0;
+
+    public Queue(int size) {
+        queue = (T[]) new Object[size];
+        start = 0;
+        end = 0;
+        count = 0;
+    }
+
+    public boolean enqueue(T element) {
+        if (isFull()) {
+            return false;
+        } else {
+            queue[end] = element;
+            end = (end + 1) % queue.length;
+        }
+        count++;
+        return true;
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            T temp = queue[start];
+            queue[start] = null;
+            start = (start + 1) % queue.length;
+            count--;
+            return temp;
+        }
+    }
+
+    public boolean isFull() {
+        return count >= queue.length;
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+}
